7fc918c81b2cd73019443e109023eaaa787f0200
dyi919 <ehddjs13579@gmail.com>
2022-12-13 03:13:26 +0900

    [setting] add Tailwind primary color, container options

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ module.exports = {
     "./src/components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      'primary': {
+        '50': '#f4f7fb',
+        '100': '#e8eff6',
+        '200': '#ccddeb',
+        '300': '#a0c1d9',
+        '400': '#6ca0c4',
+        '500': '#4a84ad',
+        '600': '#386a91',
+        '700': '#2e5576',
+        '800': '#294963',
+        '900': '#273f54',
+      },
+    },
+    containter: {
+      'center': true,
+    }
   },
   plugins: [],
 };
