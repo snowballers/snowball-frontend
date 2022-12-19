7e9a5a97d2950dccd636218337269409ed15f30b
동동이 <drdragon777@naver.com>
2022-12-20 06:41:26 +0900

    [feature] add initial town page

--- src/pages/town/index.tsx
+++ src/pages/town/index.tsx
@@ -1,15 +1,30 @@
-import type { NextPage } from 'next';
+import { NextPage } from 'next';
 
 import Container from '@components/Container';
+import BaseImage from '@components/BaseImage';
+import Title from '../../components/Title';
+import Span from '../../components/Span';
+import FlexBox from '@components/FlexBox';
 
-const Town: NextPage = () => {
+const TITLE_MOCK = "동용's 눈사람 마을";
+const SNOWMAN_NUM_MOCK = 7;
+const SNOWMAN_NUM_TEXT = '명의 눈사람이 살고 있어요';
+
+const Home: NextPage = () => {
+  const snowmanText = SNOWMAN_NUM_MOCK + SNOWMAN_NUM_TEXT;
+
   return (
     <Container bgColor="bg-primary-100">
-      <div className="pt-8">
-        <h1 className="text-4xl text-center text-primary-900">마을 페이지</h1>
-      </div>
+      <Container className="absolute z-0">
+        <BaseImage src="/image/hometown.png" alt="마을 배경 이미지" fill />
+      </Container>
+
+      <FlexBox className="z-10 justify-center items-center pt-[133px]" direction="flex-col">
+        <Title text={TITLE_MOCK} className="text-primary-50 text-3xl pb-[6px]" />
+        <Span text={snowmanText} className="text-[20px] text-primary-450" />
+      </FlexBox>
     </Container>
   );
 };
 
-export default Town;
+export default Home;
