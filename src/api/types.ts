206a19f6f8a58552b417a91126065e0bb9ce4730
dyi919 <ehddjs13579@gmail.com>
2022-12-20 20:56:43 +0900

    unify type definition

--- src/api/types.ts
+++ src/api/types.ts
@@ -1,91 +1,89 @@
-type QuestionAnswerPair = {
-    id: number;
-    answerId: number;
+interface QuestionAnswerPair {
+  id: number;
+  answerId: number;
 }
 
-type Snowman = {
-    id: number;
-    name: string;
-    imageUrl: string;
-    description: string;
+interface Snowman {
+  id: number;
+  name: string;
+  imageUrl: string;
+  description: string;
 }
 
-type TownSnowman = {
-    id: number;
-    imageUrl: string;
-    seen: boolean;
+interface TownSnowman {
+  id: number;
+  imageUrl: string;
+  seen: boolean;
 }
 
-type Question = {
-    id: number;
-    content: string;
-    totalAnswers: number;
-    answers: Answer[];
+interface Question {
+  id: number;
+  content: string;
+  totalAnswers: number;
+  answers: Answer[];
 }
 
-type Answer = {
-    id: number;
-    content: string;
+interface Answer {
+  id: number;
+  content: string;
 }
 
-type TownGetResponseData = {
-    isMine: boolean;
-    nickname: string;
-    townName: string;
-    totalSnowman: number;
-    snowmans: TownSnowman[];
+interface TownGetResponseData {
+  isMine: boolean;
+  nickname: string;
+  townName: string;
+  totalSnowman: number;
+  snowmans: TownSnowman[];
 }
 
-type SnowmanGetResponseData = {
-    senderName: string;
-    letter: string;
-    snowman: Snowman;
+interface SnowmanGetResponseData {
+  senderName: string;
+  letter: string;
+  snowman: Snowman;
 }
 
-type QuestionGetResponseData = {
-    name: string;
-    totalQuestion: number;
-    questions: Question[];
+interface QuestionGetResponseData {
+  name: string;
+  totalQuestion: number;
+  questions: Question[];
 }
 
-type QuestionPostRepsonseData = {
-    nickname: string;
-    percent: number;
-    snowman: Snowman;
+interface QuestionPostRepsonseData {
+  nickname: string;
+  percent: number;
+  snowman: Snowman;
 }
 
 interface DefaultResponse {
-    success: boolean;
-    error?: string | null;
+  success: boolean;
+  error?: string | null;
 }
 
 export interface ITownGetResponse extends DefaultResponse {
-    data: TownGetResponseData;
+  data: TownGetResponseData;
 }
 
 export interface ISnowmanGetResponse extends DefaultResponse {
-    data: SnowmanGetResponseData;
+  data: SnowmanGetResponseData;
 }
 
 export interface IQuestionGetResponse extends DefaultResponse {
-    data: QuestionGetResponseData;
+  data: QuestionGetResponseData;
 }
 
 export interface IQuestionPostRequest {
-    totalQuestion: number;
-    questions: QuestionAnswerPair[];
+  totalQuestion: number;
+  questions: QuestionAnswerPair[];
 }
 
 export interface IQuestionPostResponse extends DefaultResponse {
-    data: QuestionPostRepsonseData;
+  data: QuestionPostRepsonseData;
 }
 
 export interface ILetterPostRequest {
-    snowmanId: number;
-    sender: string;
-    letter?: string;
+  snowmanId: number;
+  sender: string;
+  letter?: string;
 }
 
-export interface ILetterPostResponse extends DefaultResponse {
-
-}
+export interface ILetterPostResponse extends DefaultResponse {}
