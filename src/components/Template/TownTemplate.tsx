89f980185be903ec89e31b14e53ef398dfa2d4d9
동동이 <drdragon777@naver.com>
2022-12-21 00:46:43 +0900

    [feature] add share feature in TownTemplate page

--- src/components/Template/TownTemplate.tsx
+++ src/components/Template/TownTemplate.tsx
@@ -3,13 +3,25 @@ import BaseImage from '@components/BaseImage';
 import FlexBox from '@components/FlexBox';
 import Arrow from '@components/icons/Arrow';
 import TownTitleBox from '@components/Town/TownTitleBox';
+import CameraBtn from '@components/Town/CameraBtn';
+import ShareBtn from '@components/Town/ShareBtn';
+import {  getCurrentUrl,  sharePage } from 'src/utils/share';
 
 type Props = {
+  title: string;
   isMe?: boolean;
 };
 
 const TownTemplate = (props: Props) => {
-  const { isMe } = props;
+  const { title, isMe } = props;
+  const direction = isMe ? 'flex-row' : 'flex-col';
+
+  const currentUrl = getCurrentUrl();
+  const handleShare = sharePage({
+    title: `snowballers | ${title}`,
+    url: currentUrl,
+  });
+
   return (
     <>
       <Container className="absolute z-0">
@@ -21,6 +33,11 @@ const TownTemplate = (props: Props) => {
       <FlexBox position="absolute" direction="flex-row" className="w-full sm:w-6/12 top-[50%] -translate-y-[50%] justify-between">
         <Arrow fill="#55b8ff" className="relative w-[24px] h-[48px] left-[33.66px]" />
         <Arrow fill="#55b8ff" className="relative w-[24px] h-[48px] rotate-180 right-[33.66px]" />
+      </FlexBox>
+
+      <FlexBox position="fixed" direction={direction} className="w-full sm:w-6/12 bottom-[50px] pr-[21px] pl-[21px]">
+        <CameraBtn />
+        <ShareBtn onClick={handleShare}/>
       </FlexBox>
     </>
   );
--- src/pages/town/index.tsx
+++ src/pages/town/index.tsx
@@ -6,7 +6,7 @@ import TownTemplate from '@components/Template/TownTemplate';
 const Town: NextPage = () => {
   return (
     <Container>
-      <TownTemplate isMe />
+      <TownTemplate isMe title='내 마을보기' />
     </Container>
   );
 };
