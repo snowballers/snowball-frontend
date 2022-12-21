ea2cbc9e361fbe71d663db55e71b5ca114ccb020
dyi919 <ehddjs13579@gmail.com>
2022-12-22 03:10:03 +0900

    [feature] animate progress bar

--- src/pages/question/components/ProgressBar.tsx
+++ src/pages/question/components/ProgressBar.tsx
@@ -1,28 +1,13 @@
 import Image from 'next/image';
-import { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import { ProgressBarProps } from '../types';
 
 const ProgressBar = ({ finished, questionNo, totalQuestions }: ProgressBarProps) => {
   const [percentage, setPercentage] = useState<string>();
-  const [left, setLeft] = useState<number>();
 
+  const totalProgressRef = useRef<HTMLDivElement>(null);
   const currentProgressRef = useRef<HTMLDivElement>(null);
-
-  function handleResize() {
-    if (currentProgressRef.current === null) return;
-    setLeft(currentProgressRef.current.clientWidth - 48);
-  }
-
-  useEffect(() => {
-    window.addEventListener('resize', handleResize);
-
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
-
-  useLayoutEffect(() => {
-    handleResize();
-  }, []);
 
   useEffect(() => {
     const getPercentage = (questionNo: number, totalQuestions: number): number => {
@@ -32,15 +17,18 @@ const ProgressBar = ({ finished, questionNo, totalQuestions }: ProgressBarProps)
     setPercentage(`${getPercentage(questionNo, totalQuestions)}%`);
   }, [questionNo, totalQuestions, finished]);
 
-  useEffect(() => {
-    handleResize();
-  }, [percentage]);
-
   return (
     <>
-      <div className="overflow-visible relative w-full mt-6 bg-primary-200 rounded-full h-2.5">
-        <div ref={currentProgressRef} className="bg-primary-600 h-2.5 rounded-full" style={{ width: percentage }}></div>
-        <Image className="absolute -top-[24px]" style={{ left: left }} src="/roll-cropped.gif" alt="roll" width="64" height="32" />
+      <div ref={totalProgressRef} className="overflow-visible relative w-full mt-6 bg-primary-200 rounded-full h-2.5">
+        <div ref={currentProgressRef} className="bg-primary-600 h-2.5 rounded-full" style={{ width: percentage, transition: 'width 1s linear' }}></div>
+        <Image
+          className="absolute -top-[24px]"
+          style={{ left: `calc(${percentage} - 48px)`, transition: '1s linear' }}
+          src="/roll-cropped.gif"
+          alt="roll"
+          width="64"
+          height="32"
+        />
       </div>
       <div className="flex justify-between mt-2">
         <span></span>
