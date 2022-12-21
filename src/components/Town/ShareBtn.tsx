dfe18d3124bada044432167eebc3b5166587385a
동동이 <drdragon777@naver.com>
2022-12-21 14:16:25 +0900

    [chore] move share login to ShareBtn component

--- src/components/Town/ShareBtn.tsx
+++ src/components/Town/ShareBtn.tsx
@@ -1,11 +1,20 @@
+import { getCurrentUrl, sharePage } from "src/utils/share";
+
 type Props = {
-  onClick?: () => void;
+  title: string;
 };
 
 function ShareBtn(props: Props) {
-  const { onClick } = props;
+  const { title } = props;
+
+  const currentUrl = getCurrentUrl();
+  const handleShare = sharePage({
+    title: `snowballers | ${title}`,
+    url: currentUrl,
+  });
+  
   return (
-    <button type="button" className="absolute left-[50%] -translate-x-[50%] top-[50%] -translate-y-[50%] text-[22px] text-primary-900" onClick={onClick}>
+    <button type="button" className="absolute left-[50%] -translate-x-[50%] top-[50%] -translate-y-[50%] text-[22px] text-primary-900" onClick={handleShare}>
       내 마을 링크 공유하기
     </button>
   );
--- src/pages/town/index.tsx
+++ src/pages/town/index.tsx
@@ -1,14 +1,9 @@
 import { NextPage } from 'next';
 
-import Container from '@components/Container';
 import TownTemplate from '@components/Template/TownTemplate';
 
 const Town: NextPage = () => {
-  return (
-    <Container>
-      <TownTemplate isMe title='내 마을보기' />
-    </Container>
-  );
+  return <TownTemplate isMe title="내 마을보기" />;
 };
 
 export default Town;
