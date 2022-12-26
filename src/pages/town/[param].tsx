0787afac5bb1535a4424e9986eceac05d8b95a22
동동이 <drdragon777@naver.com>
2022-12-26 20:04:09 +0900

    [chore] change file name

--- src/pages/town/[param].tsx
+++ src/pages/town/[param].tsx
@@ -2,11 +2,15 @@ import { NextPage } from 'next';
 
 import TownTemplate from '@components/Template/TownTemplate';
 import Container from '@components/Container';
+import { useRouter } from 'next/router';
 
 const Town: NextPage = () => {
+  const router = useRouter();
+  const { param } = router.query;
+
   return (
     <Container>
-      <TownTemplate title="눈사람 마을보기" />
+      <TownTemplate title="눈사람 마을보기" url={param as string} />
     </Container>
   );
 };
