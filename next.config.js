79fd4655067a67975176c0cb339dbdb11a68386f
동동이 <drdragon777@naver.com>
2022-12-26 20:05:24 +0900

    [chore] add oauth url env

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const nextConfig = {
   swcMinify: true,
   env: {
     API_BASE_URL: process.env.API_BASE_URL,
+    KAKAO_AUTH_URL: process.env.KAKAO_AUTH_URL,
+    GOOGLE_AUTH_URL: process.env.GOOGLE_AUTH_URL,
   },
 };
 
