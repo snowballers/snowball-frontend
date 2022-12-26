54a5b6d65fba31c887251f12fe275b81e908c5f6
dyi919 <ehddjs13579@gmail.com>
2022-12-26 18:10:22 +0900

    [refactor] remove unnecessary codes, fix scroll button bug

--- src/components/Result/ResultImageText.tsx
+++ src/components/Result/ResultImageText.tsx
@@ -1,14 +1,14 @@
 import Image from 'next/image';
-import { useRouter } from 'next/router';
 
 import Title from '@components/Title';
 import LetterInputBox from './LetterInputBox';
 import Button from '@components/Button';
 import { Snowman } from '@api/types';
 import Snowflake from '@components/Snowflake';
+import { RefObject } from 'react';
 
 type Props = {
-  sender: string;
+  scrollRef: RefObject<HTMLDivElement>;
   nickname: string;
   percent: number;
   letter: string;
@@ -17,9 +17,9 @@ type Props = {
   createLetter: any;
 };
 
-const ResultImageText = ({ sender, nickname, percent, snowman, letter, setLetter, createLetter }: Props) => {
+const ResultImageText = ({ scrollRef, nickname, percent, snowman, letter, setLetter, createLetter }: Props) => {
   return (
-    <div className="absolute inset-x-0 z-50" style={{ background: 'rgb(237, 239, 247)' }}>
+    <div ref={scrollRef} className="absolute inset-x-0 z-50" style={{ background: 'rgb(237, 239, 247)' }}>
       <Snowflake />
       <Image className="absolute -z-[10] w-full h-100vh" src="/image/hometown.png" alt="마을 배경 이미지" width="0" height="0" sizes="100vw" />
       <Title text={`짠! ${nickname}님을 위한`} className="text-center pt-20 text-primary-50 text-2xl inset-x-0" />
@@ -27,10 +27,7 @@ const ResultImageText = ({ sender, nickname, percent, snowman, letter, setLetter
       <div className="inset-x-0">
         <Image className="mx-auto" src={`/snowman/${snowman?.type}.png`} alt="" width={350} height={350} />
       </div>
-      <p className="text-center text-xl">
-        {sender}님이 만든 눈사람은
-        <br /> <span className="text-primary-600">{snowman?.name}</span>이에요!
-      </p>
+      <p className="text-center text-xl text-primary-600">{snowman?.name}</p>
       <p className="text-center text-sm text-gray-400 pt-8">
         <span className="text-primary-600">{percent}%</span>의 사람들이 {nickname}님을
       </p>
