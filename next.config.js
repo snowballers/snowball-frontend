fdd0659106b0ce4c4e43e0a12230bdb9f85668e0
동동이 <drdragon777@naver.com>
2022-12-28 21:34:21 +0900

    [feature] add envs

--- next.config.js
+++ next.config.js
@@ -6,6 +6,8 @@ const nextConfig = {
     API_BASE_URL: process.env.API_BASE_URL,
     KAKAO_AUTH_URL: process.env.KAKAO_AUTH_URL,
     GOOGLE_AUTH_URL: process.env.GOOGLE_AUTH_URL,
+    OAUTH_REDIRECT_URI: process.env.OAUTH_REDIRECT_URI,
+    OAUTH_CLIENT_ID: process.env.OAUTH_CLIENT_ID,
   },
 };
 
