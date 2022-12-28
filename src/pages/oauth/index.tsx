a8bd77900da5e4b34979e7822f8c98ea28e17bd3
동동이 <drdragon777@naver.com>
2022-12-28 21:35:03 +0900

    [feature] add send auth code logic

--- src/pages/oauth/index.tsx
+++ src/pages/oauth/index.tsx
@@ -2,8 +2,23 @@ import type { NextPage } from 'next';
 
 import Container from '@components/Container';
 import { Loading } from '@components/Question';
+import { useRouter } from 'next/router';
+import { usePostAuthCode } from '@hooks/api/useAuth';
+import { useEffect } from 'react';
 
 const Oauth: NextPage = () => {
+  const {
+    isReady,
+    query: { code },
+  } = useRouter();
+  const { mutate } = usePostAuthCode();
+
+  useEffect(() => {
+    if (isReady && code && typeof code === 'string') {
+      mutate({ provider: 'kakao', code });
+    }
+  }, [mutate, code, isReady]);
+
   return (
     <Container bgColor="bg-primary-100">
       <Loading type="loading" text="사용자 확인 중" />
