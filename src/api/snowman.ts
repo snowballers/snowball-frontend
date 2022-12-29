12fd8e3b25605c7b2fe90b706e5c932eaf5c4860
dyi919 <ehddjs13579@gmail.com>
2022-12-29 16:47:44 +0900

    [hotfix] change read snowman api bug

--- src/api/snowman.ts
+++ src/api/snowman.ts
@@ -1,7 +1,6 @@
 import { api } from './index';
-import { ISnowmanGetResponse } from './types';
 
 export async function readSnowman(id: string) {
   const response = await api.get(`/snowman/${id}`);
-  return response.data;
+  return response;
 }
