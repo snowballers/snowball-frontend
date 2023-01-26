a6dbc64cb83d64239fe03cbcf2b0700734961646
dyi919 <ehddjs13579@gmail.com>
2023-01-26 14:58:03 +0900

    [hotfix] fix og image url

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css';
-import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import type { AppProps } from 'next/app';
 import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Toast from '@components/Toast';
 
@@ -19,16 +19,15 @@ export const queryClient = new QueryClient({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Toast />
       <Head>
         <title>눈사람 마을</title>
         <meta property="og:url" content="www.snowtown.team" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="눈사람 마을" />
         <meta property="og:description" content="친구들이 만들어주는 날 닮은 눈사람들" />
-        <meta property="og:image" content="/image/roll-cropped.gif" />
+        <meta property="og:image" content="/roll-cropped.gif" />
       </Head>
-
+      <Toast />
       <QueryClientProvider client={queryClient}>
         <Hydrate state={pageProps.dehydratedState}>
           <Component {...pageProps} />
