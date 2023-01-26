1c36aba8e39ce0de525734ee4db3690cbb2c285c
dyi919 <ehddjs13579@gmail.com>
2023-01-26 14:50:04 +0900

    [feature] add meta tag og attributes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
-import type {  AppProps } from 'next/app';
-import {  Hydrate,  QueryClient,  QueryClientProvider } from '@tanstack/react-query';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Toast from '@components/Toast';
 
 export const queryClient = new QueryClient({
@@ -19,6 +20,15 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Toast />
+      <Head>
+        <title>눈사람 마을</title>
+        <meta property="og:url" content="www.snowtown.team" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="눈사람 마을" />
+        <meta property="og:description" content="친구들이 만들어주는 날 닮은 눈사람들" />
+        <meta property="og:image" content="/image/roll-cropped.gif" />
+      </Head>
+
       <QueryClientProvider client={queryClient}>
         <Hydrate state={pageProps.dehydratedState}>
           <Component {...pageProps} />
